d42e58149955304fd21dddf0c4c82563f416c12f
sjoseph1848 <sjoseph1848@gmail.com>
2020-07-30 20:38:23 -0500

    added more validation for the form and button

---
+++ src/app/models/StockSymbol.ts
@@ -0,0 +1,3 @@
+export interface StockSymbol {
+    stockSymbol: string;
+}
--- src/app/stock-form/stock-form.component.ts
+++ src/app/stock-form/stock-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StockSymbol } from '../models/StockSymbol';
 
 @Component({
   selector: 'app-stock-form',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-form.component.scss']
 })
 export class StockFormComponent implements OnInit {
+  originalSymbol: StockSymbol = {
+    stockSymbol: 'AAPL'
+  }
+  validSymbol: boolean = false;
+
+  stockSymbol: StockSymbol = { ...this.originalSymbol };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddStockForm(form: NgForm) {
+    if (form.valid) {
+      this.validSymbol = true;
+    } else {
+      this.validSymbol = false;
+    }
+    console.log('in on Submit: ', form.valid);
+    console.log('in on Submit: ', form.value.theSymbol);
+
   }
 
 }
