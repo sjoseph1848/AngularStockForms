b6cc52c6b2b5f5ead268bcc417e37aaeea2a85e7
sjoseph1848 <sjoseph1848@gmail.com>
2020-07-30 20:19:42 -0500

    added the basis for the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule, MatCardModule, MatButtonModule, } from '@angular/material';
+import { StockFormComponent } from './stock-form/stock-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
